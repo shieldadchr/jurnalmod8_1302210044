93a0fba67b660331d8f10a501ba1d38e4ae2f97f
shieldadchr <shielda.diasavitri@gmail.com>
2023-04-12 16:46:27 +0700

    Initial commit

---
+++ jurnalmod8_1302210044/Bank Transfer Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod8_1302210044
+{
+    internal class Bank_Transfer_Config
+    {
+    }
+}
---
+++ jurnalmod8_1302210044/Program.cs
@@ -0,0 +1,80 @@
+﻿using modul8_1302213062;
+using System;
+using System.Collections.Generic;
+
+namespace modul8_1302213062
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankTransferConfig.LoadConfig();
+
+
+            string lang = BankTransferConfig.GetLang();
+
+            if (lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+            else
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+
+
+            int jumlah = int.Parse(Console.ReadLine());
+            int biayaTransfer;
+
+            if (jumlah <= BankTransferConfig.GetThreshold())
+            {
+                biayaTransfer = BankTransferConfig.GetLowFee();
+            }
+            else
+            {
+                biayaTransfer = BankTransferConfig.GetHighFee();
+            }
+
+            int nominalTransfer = jumlah + biayaTransfer;
+
+
+            if (BankTransferConfig.GetLang() == "en")
+            {
+                Console.WriteLine($"Transferfee = {biayaTransfer}\nTotal amount = {nominalTransfer}");
+            }
+            else
+            {
+                Console.WriteLine($"Biaya transfer = {biayaTransfer}\nTotal biaya = {nominalTransfer}");
+            }
+
+            if (BankTransferConfig.GetLang() == "en")
+            {
+                Console.WriteLine("Select transfer method:");
+            }
+            else if (BankTransferConfig.GetLang() == "id")
+            {
+                Console.WriteLine("Pilih metode transfer:");
+            }
+
+            List<string> transferMethods = BankTransferConfig.GetMethods();
+
+
+            if (BankTransferConfig.GetLang() == "en")
+
+                string confirmation = Console.ReadLine();
+
+            if (confirmation == BankTransferConfig.GetConfirmation(BankTransferConfig.GetLang()))
+            {
+
+            }
+            else
+            {
+                if (BankTransferConfig.GetLang() == "en")
+
+    }
+        }
