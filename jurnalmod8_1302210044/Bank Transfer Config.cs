0edb8c8d89c03ed18d31eda5d293f2341d6bca1b
shieldadchr <shielda.diasavitri@gmail.com>
2023-04-12 17:07:41 +0700

    aplikasi bank transfer dengan runtime configuration

--- jurnalmod8_1302210044/Bank Transfer Config.cs
+++ jurnalmod8_1302210044/Bank Transfer Config.cs
@@ -1,12 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace jurnalmod8_1302210044
+
 {
-    internal class Bank_Transfer_Config
+    internal class BankTransferConfig
     {
+        public string lang { get; set; }
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+        public string meth1 { get; set; }
+        public string meth2 { get; set; }
+        public string meth3 { get; set; }
+        public string meth4 { get; set; }
+
+        public string en { get; set; }
+        public string id { get; set; }
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, int threshold,
+            int low_fee, int high_fee, string meth1, string meth2,
+            string meth3, string meth4, string en, string id)
+        {
+            this.lang = lang;
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+            this.meth1 = meth1;
+            this.meth2 = meth2;
+            this.meth3 = meth3;
+            this.meth4 = meth4;
+            this.en = en;
+            this.id = id;
+        }
+
+        public void changeLang_1302210044()
+        {
+            if (lang == "en")
+            {
+                lang = "id";
+            }
+            else
+            {
+                lang = "en";
+            }
+        }
+    }
+
+    class readJson
+    {
+        public BankTransferConfig bankTfCon;
+        public const string readFile = "D:\\KONSTRUKSI PERANGKAT LUNAK\\jurnalmod8_1302210044\\jurnalmod8_1302210044\\Bank Transfer Config.json" +
+            "D:\\KONSTRUKSI PERANGKAT LUNAK\\jurnalmod8_1302210044\\jurnalmod8_1302210044\\Bank Transfer Config.cs"
+            ;
+        public readJson()
+        {
+            try
+            {
+                readConfigFile_1302210044();
+            }
+            catch (Exception)
+            {
+                setDefault_1302210044();
+                writeNewConfigFile_1302210044();
+            }
+        }
+
+        private BankTransferConfig readConfigFile_1302210044()
+        {
+            string configJson = File.ReadAllText(readFile);
+            bankTfCon = JsonSerializer.Deserialize<BankTransferConfig>(configJson);
+            return bankTfCon;
+        }
+
+        private void setDefault_1302210044()
+        {
+            int threshold = 25000000;
+            int low_fee = 6500;
+            int high_fee = 15000;
+            string meth1 = "1. RTO (real-time)";
+            string meth2 = "2. SKN";
+            string meth3 = "3. RTGS";
+            string meth4 = "4. BI FAST";
+            string en = "yes";
+            string id = "ya";
+            bankTfCon = new BankTransferConfig("id", threshold, low_fee,
+                high_fee, meth1, meth2, meth3, meth4, en, id);
+        }
+
+        private void writeNewConfigFile_1302210044()
+        {
+            JsonSerializerOptions opt = new JsonSerializerOptions() { WriteIndented = true };
+
+            string jsonStr = JsonSerializer.Serialize(bankTfCon, opt);
+        }
     }
 }
--- jurnalmod8_1302210044/Program.cs
+++ jurnalmod8_1302210044/Program.cs
@@ -1,80 +1,46 @@
-﻿using modul8_1302213062;
-using System;
-using System.Collections.Generic;
+﻿using jurnalmod8_1302210044;
+using System.Security.Cryptography.X509Certificates;
 
-namespace modul8_1302213062
+BankTransferConfig tfConfig = new BankTransferConfig();
+readJson read = new readJson();
+
+// read.bankTfCon.changeLang_1302210044();
+
+// part i & ii
+Console.WriteLine("Please insert the amount of money to transfer:");
+int mon = int.Parse(Console.ReadLine());
+
+int sum = 0;
+if (mon <= read.bankTfCon.threshold)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    sum = mon + read.bankTfCon.low_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.low_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+}
+else
+{
+    sum = mon + read.bankTfCon.high_fee;
+    Console.WriteLine("Transfer fee = Rp " + read.bankTfCon.high_fee);
+    Console.WriteLine("Total amount = Rp " + sum);
+}
 
-            BankTransferConfig.LoadConfig();
+// part iii & iv
+Console.WriteLine("\nSelect transfer method: ");
+Console.WriteLine(read.bankTfCon.meth1);
+Console.WriteLine(read.bankTfCon.meth2);
+Console.WriteLine(read.bankTfCon.meth3);
+Console.WriteLine(read.bankTfCon.meth4);
+string meth = Console.ReadLine();
 
+// part v - vii
+Console.WriteLine("\nPlease type " + read.bankTfCon.en + " to confirm the transaction");
+string confirm = Console.ReadLine();
 
-            string lang = BankTransferConfig.GetLang();
-
-            if (lang == "en")
-            {
-                Console.WriteLine("Please insert the amount of money to transfer:");
-            }
-            else if (lang == "id")
-            {
-                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
-            }
-            else
-            {
-                Console.WriteLine("Please insert the amount of money to transfer:");
-            }
-
-
-            int jumlah = int.Parse(Console.ReadLine());
-            int biayaTransfer;
-
-            if (jumlah <= BankTransferConfig.GetThreshold())
-            {
-                biayaTransfer = BankTransferConfig.GetLowFee();
-            }
-            else
-            {
-                biayaTransfer = BankTransferConfig.GetHighFee();
-            }
-
-            int nominalTransfer = jumlah + biayaTransfer;
-
-
-            if (BankTransferConfig.GetLang() == "en")
-            {
-                Console.WriteLine($"Transferfee = {biayaTransfer}\nTotal amount = {nominalTransfer}");
-            }
-            else
-            {
-                Console.WriteLine($"Biaya transfer = {biayaTransfer}\nTotal biaya = {nominalTransfer}");
-            }
-
-            if (BankTransferConfig.GetLang() == "en")
-            {
-                Console.WriteLine("Select transfer method:");
-            }
-            else if (BankTransferConfig.GetLang() == "id")
-            {
-                Console.WriteLine("Pilih metode transfer:");
-            }
-
-            List<string> transferMethods = BankTransferConfig.GetMethods();
-
-
-            if (BankTransferConfig.GetLang() == "en")
-
-                string confirmation = Console.ReadLine();
-
-            if (confirmation == BankTransferConfig.GetConfirmation(BankTransferConfig.GetLang()))
-            {
-
-            }
-            else
-            {
-                if (BankTransferConfig.GetLang() == "en")
-
-    }
-        }
+if (confirm == read.bankTfCon.en)
+{
+    Console.WriteLine("The transfer is completed");
+}
+else
+{
+    Console.WriteLine("Transfer is cancelled");
+}
